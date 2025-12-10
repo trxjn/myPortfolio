018b1c6779ea996dbc01f3d0ad939ea82409d26b
trxjn <trixiaaa1@gmail.com>
2025-12-10 11:57:58 +0800

    Fix PostCSS config for Vite

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
-module.exports = {
-  plugins: [
-    require('tailwindcss'),  // Use the version bundled with react-scripts
-    require('autoprefixer'),  // Autoprefixer for cross-browser compatibility
-  ],
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
